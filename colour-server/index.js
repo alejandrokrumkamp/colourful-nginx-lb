43e85aef8acb623c18e6d867dc7be0af73709c71
Alejandro Krumkamp <alekd9@gmail.com>
2018-11-02 21:59:09 -0300

    Removed Dockerfile

--- colour-server/index.js
+++ colour-server/index.js
@@ -1,17 +1,18 @@
-
 const express = require('express');
 const app = express();
 const port = process.argv[2];
 const colour = process.argv[3]
+let hitCount = 0;
 
 if(typeof port === 'undefined' || typeof colour === 'undefined')
 	throw 'Port and colour must be passed as node parameters';
 
 app.get('/', function (req, res) {
+  hitCount++;
   res.set('Content-Type', 'text/html');
-  res.send(new Buffer(`<body style="background: ${colour}"></body>`));
+  res.send(new Buffer(`<body style="background: ${colour}"><h1>${hitCount}</h1></body>`));
 });
 
 app.listen(port, function () {
-  console.log(colour+' server is running on port '+port);
+  console.log(colour+' server is running on http://localhost:'+port);
 });
---
+++ nginx/copyNginxCfg.sh
@@ -0,0 +1,3 @@
+sudo rm /etc/nginx/nginx.conf
+sudo mv nginx.conf /etc/nginx/nginx.conf
+sudo nginx -s reload
